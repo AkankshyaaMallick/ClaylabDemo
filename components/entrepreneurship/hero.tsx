169cbd0de96804e1498dfe8921b0730138de70a3
AkankshyaaMallick <akankshyamallick462@gmail.com>
2025-09-07 23:01:41 +0530

    Update hero.tsx

--- components/entrepreneurship/hero.tsx
+++ components/entrepreneurship/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
     // <section className="relative w-full h-[80vh] sm:h-screen flex flex-col items-center overflow-hidden">
     <section className="relative w-full h-[80vh] sm:h-screen flex flex-col items-center overflow-hidden">
       <Image
-        src="/claylab assets/general/HeroSectionImages/IMG_5000.jpg"
+        src="/claylab assets/general/HeroSectionImages/hero.jpg"
         alt="Background"
         fill
         className="object-cover object-center "
